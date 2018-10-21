f6ded9952de31b300b80d9f993b193d4be67b210
wukia <wukai@lbesec.com>
2018-10-21 23:09:02 +0800

    提交webrtc demo

--- LBE_demo/server.js
+++ LBE_demo/server.js
@@ -1,12 +1,48 @@
-const https = require('https');
-const fs = require('fs');
+var express = require('express'),
+app = express(),
+server = require('http').createServer(app);
 
-const options = {
-  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
-};
+server.listen(3000);
 
-https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-}).listen(8000)
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/webrtc.html');
+});
+
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({server: server});
+
+// 存储socket的数组，这里只能有2个socket，每次测试需要重启，否则会出错
+var wsc = [],
+index = 1;
+
+// 有socket连入
+wss.on('connection', function(ws) {
+    console.log('connection');
+
+    // 将socket存入数组
+    wsc.push(ws);
+
+    // 记下对方socket在数组中的下标，因为这个测试程序只允许2个socket
+    // 所以第一个连入的socket存入0，第二个连入的就是存入1
+    // otherIndex就反着来，第一个socket的otherIndex下标为1，第二个socket的otherIndex下标为0
+    var otherIndex = index--,
+    desc = null;
+
+    if (otherIndex == 1) {
+        desc = 'first socket';
+    } else {
+        desc = 'second socket';
+    }
+
+    // 转发收到的消息
+    ws.on('message', function(message) {
+        var json = JSON.parse(message);
+        console.log('received (' + desc + '): ', json);
+
+        wsc[otherIndex].send(message, function (error) {
+            if (error) {
+                console.log('Send message error (' + desc + '): ', error);
+            }
+        });
+    });
+});
