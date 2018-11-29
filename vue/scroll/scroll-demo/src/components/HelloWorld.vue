3baa563f8a0d7067d4e9b93b7fe80c9e707cc4e9
wukia <wukai@lbesec.com>
2018-11-29 11:40:26 +0800

    修改。。。。vuescroll

---
+++ vue/scroll/scroll-demo/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <RecycleScroller
+    class="scroller"
+    :items="list"
+    :item-height="32"
+  >
+    <div slot-scope="{ item }" class="user">
+      {{ item.name }}
+    </div>
+  </RecycleScroller>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: new Array(10000),
+      list: []
+    }
+  },
+  created() {
+    for(let [i, item] of Object.entries(this.arr)) {
+      this.list.push({
+        id: i,
+        name: 'wukai'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.scroller {
+  height: 100%;
+}
+
+.user {
+  height: 32%;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+}
+</style>
